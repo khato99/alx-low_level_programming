fcf92371705df7e94ad2523a11fab343127dd9ee
Wisdom Robinson <wisdomgrreatrobinson@gmail.com>
2022-12-15 05:00:55 -0800

    LEap year

--- 0x03-debugging/3-print_remaining_days.c
+++ 0x03-debugging/3-print_remaining_days.c
@@ -6,17 +6,14 @@
  * @month: month in number format
  * @day: day of month
  * @year: year
+ * Return: void
  */
 
 void print_remaining_days(int month, int day, int year)
 {
-	/**
-	 * leap year when it can be divided 
-	 * by 100 and 400 or by 4 evenly
-	 */
-	if ((year % 100 == 0 && year % 400 == 0) || (year % 4 == 0))
+	if ((year % 4 == 0) && (year % 400 == 0 || year % 100 != 0))
 	{
-		if (month > 2 && day >= 60)
+		if (month > 3 && day >= 60)
 		{
 			day++;
 		}
