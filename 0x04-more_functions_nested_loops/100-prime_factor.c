9eb39445bea2d20c8fd231fcc48642d3c37f6261
Wisdom Robinson <wisdomgrreatrobinson@gmail.com>
2022-12-15 17:03:16 -0800

    100-prime factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	unsigned long int i, n = 612852475243;
+	unsigned long int i, n = 612852475143;
 
 	for (i = 3; i < 782849; i = i + 2)
 	{
@@ -16,5 +16,6 @@ int main(void)
 			n = n / i;
 	}
 	printf("%lu\n", n);
+
 	return (0);
 }
