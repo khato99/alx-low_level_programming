ce1cf067ff140a67473820f03cb6b05b96a481bc
Wisdom Robinson <wisdomgrreatrobinson@gmail.com>
2022-12-19 10:32:23 -0800

    I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-main.c
@@ -0,0 +1,14 @@
+nclude "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0..
+ */
+{
+	char *str;
+
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	print_rev(str);
+	return (0);
+}
