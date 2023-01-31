5b353cf9b062147192423a2e6ec4ff921243dcc9
Wisdom Robinson <wisdomgrreatrobinson@gmail.com>
2023-01-31 02:45:20 -0800

    missing error in task 0 of C13

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,50 +1,21 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
- * printnum - print number
- * @a: number to be printed
+ * print_listint - prints all the elements of a linked list
+ * @h: head of the list
  *
+ * Return: the number of nodes
  */
-
-void printnum(int a)
-{
-/* if number is smaller than 0 put a '-' sign */
-/* and change number to positive */
-if (a < 0)
-{
-putchar('-');
-a = -a;
-}
-
-/*recur till uou remove all last didgit*/
-if (a / 10)
-printnum(a / 10);
-
-/* print the last digit */
-putchar(a % 10 + '0');
-}
-
-
-/**
- * print_listint - print the elements of all nodes
- * @h: address to head of linked list
- * Return: number of nodes
- */
-
 size_t print_listint(const listint_t *h)
 {
-int i = 0;
-if (h == NULL)
-return (0);
+const listint_t *cursor = h;
+size_t count = 0;
 
-while (h != NULL)
+while (cursor != NULL)
 {
-printnum(h->n);
-h = h->next;
-putchar('\n');
-i++;
+printf("%d\n", cursor->n);
+count += 1;
+cursor = cursor->next;
 }
-return (i);
+return (count);
 }
