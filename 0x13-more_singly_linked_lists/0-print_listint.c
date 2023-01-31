5a69464f37f0288eef5c0a6141a6e924af073388
Wisdom Robinson <wisdomgrreatrobinson@gmail.com>
2023-01-31 03:45:59 -0800

    TASK 0 c13

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,21 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
 #include "lists.h"
 
 /**
- * print_listint - prints all the elements of a linked list
- * @h: linked list of type listint_t to print
+ * printnum - print number
+ * @a: number to be printed
  *
+ */
+
+void printnum(int a)
+{
+/* if number is smaller than 0 put a '-' sign */
+/* and change number to positive */
+if (a < 0)
+{
+putchar('-');
+a = -a;
+}
+
+/*recur till uou remove all last didgit*/
+if (a / 10)
+printnum(a / 10);
+
+/* print the last digit */
+putchar(a % 10 + '0');
+}
+
+
+/**
+ * print_listint - print the elements of all nodes
+ * @h: address to head of linked list
  * Return: number of nodes
  */
+
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+int i = 0;
+if (h == NULL)
+return (0);
 
-	while (h)
-	{
-		nodes++;
-		printf("%d\n", h->n);
-		h = h->next;
-	}
-
-	return (nodes);
+while (h != NULL)
+{
+printnum(h->n);
+h = h->next;
+putchar('\n');
+i++;
+}
+return (i);
 }
